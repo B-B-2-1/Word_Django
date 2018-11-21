b3d7f7a8e65a06122bc2887cbf3517ab2577508c
B-B-2-1 <39325928+B-B-2-1@users.noreply.github.com>
2018-11-21 21:17:41 +0530

    added 3x3 buttons added 3x3button functionality in main menu

--- app/src/main/java/com/forfun/febie/word_django/MainMenuActivity.java
+++ app/src/main/java/com/forfun/febie/word_django/MainMenuActivity.java
@@ -2,6 +2,7 @@ package com.forfun.febie.word_django;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,17 +18,26 @@ public class MainMenuActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        ///////////////////////////////////////////////////////////////
+        Button button3x3=findViewById(R.id.but3x3);
+        button3x3.setOnClickListener(this);
         Button button4x4=(Button) findViewById(R.id.but4x4);
         button4x4.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+
+        switch (v.getId())
+        {
+            case R.id.but3x3:
+
+                Intent intent = new Intent(this,Game_3x3.class);
+                startActivity(intent);break;
             case R.id.but4x4:
-            Intent intent = new Intent(this, Game_4x4.class);
-            startActivity(intent);break;
+                Intent intent = new Intent(this, Game_4x4.class);
+                startActivity(intent);break;
         }
+
     }
 }
