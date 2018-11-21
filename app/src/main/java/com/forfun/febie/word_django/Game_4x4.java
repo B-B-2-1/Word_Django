0eb649c94142fd0969699a072f5426ed77303081
B-B-2-1 <39325928+B-B-2-1@users.noreply.github.com>
2018-11-21 15:36:18 +0530

    started 4x4 page design

---
+++ app/src/main/java/com/forfun/febie/word_django/Game_4x4.java
@@ -0,0 +1,13 @@
+package com.forfun.febie.word_django;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game_4x4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_4x4);
+    }
+}
